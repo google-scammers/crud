e965d60a78f0fc4a1fac9bcab94a94d818843a95
nearworld <artemismars2@outlook.com>
2023-07-11 16:51:22 +0900

    feat: navigation 기능 추가

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to={'signup'}>회원 가입</Link>
+        </li>
+        <li>
+          <Link to={'signin'}>로그인</Link>
+        </li>
+        <li>
+          <Link to={''}>게시판</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
