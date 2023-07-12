3823712e5ac12c051f1321f702f35058f0720f3d
codnr <wnd75913@gmail.com>
2023-07-12 10:57:34 +0900

    feat: reset CSS GlobelStyle 사용하여 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'routes/router';
 
 import './App.css';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>
 }
 
 export default App;
