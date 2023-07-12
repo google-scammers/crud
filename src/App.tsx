81772c929a1d84387f26b6df8fa07f1e3a87f2d6
nearworld <artemismars2@outlook.com>
2023-07-12 10:25:39 +0900

    fix: App css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'routes/router';
 
+import './App.css';
+
 function App() {
   return <RouterProvider router={router} />;
 }
