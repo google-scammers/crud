b1772f2dfe02eea543d1e7060c5a76e90e422736
codnr <wnd75913@gmail.com>
2023-07-19 11:47:47 +0900

    feat: Login page에 input 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,45 @@
+import { useInput } from 'hooks/useInput';
+import { styled } from 'styled-components';
+
 import { AuthForm } from 'components/AuthForm';
+import { AuthInput } from 'components/AuthInput';
+
+const InputContainer = styled.div``;
 
 export const Login = () => {
+  const {
+    inputValue: emailInputValue,
+    onChange: onChangeEmail,
+    validation: emailValidation,
+  } = useInput('', 'email');
+  const {
+    inputValue: passwordInputValue,
+    onChange: onChangePassword,
+    validation: passwordValidation,
+  } = useInput('', 'password');
   return (
     <div>
-      login
       <AuthForm>
-        <h1>제목</h1>
-        <p> 내용 </p>
+        <InputContainer>
+          <AuthInput
+            type="email"
+            label="email"
+            id="email"
+            name="email"
+            onChange={onChangeEmail}
+            value={emailInputValue}
+            bottomText={emailValidation.errorMessage}
+          />
+          <AuthInput
+            type="password"
+            label="password"
+            id="password"
+            name="password"
+            onChange={onChangePassword}
+            value={passwordInputValue}
+            bottomText={passwordValidation.errorMessage}
+          ></AuthInput>
+        </InputContainer>
       </AuthForm>
     </div>
   );
