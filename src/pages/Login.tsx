0b61c2a44988ab98a79a488640649774bc75d3fd
codnr <wnd75913@gmail.com>
2023-07-19 14:29:18 +0900

    design: login form 디자인 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,9 +3,25 @@ import { styled } from 'styled-components';
 
 import { AuthForm } from 'components/AuthForm';
 import { AuthInput } from 'components/AuthInput';
+import { SubmitButton } from 'components/SubmitButton';
+import { colors } from 'constants/colors';
 
 const InputContainer = styled.div`
   width: 80%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 52px;
+`;
+
+const SignUpButton = styled.div`
+  width: 100%;
+  text-align: end;
+  color: ${colors.grey600};
 `;
 
 export const Login = () => {
@@ -42,7 +58,11 @@ export const Login = () => {
             value={passwordInputValue}
             bottomText={passwordValidation.errorMessage}
           />
+          <SignUpButton>회원가입</SignUpButton>
         </InputContainer>
+        <ButtonWrapper>
+          <SubmitButton text="로그인" />
+        </ButtonWrapper>
       </AuthForm>
     </div>
   );
