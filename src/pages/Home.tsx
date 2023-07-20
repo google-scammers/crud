3c5efbd7c2f5e14886cb342522dc1d68cdc5cc80
nearworld <artemismars2@outlook.com>
2023-07-20 16:28:03 +0900

    feat: 액세스 토큰을 이용한 자동 로그인 기능 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,38 @@
+import { AxiosError } from 'axios';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-import { ArticleModal } from 'components/ArticleModal';
 import { Navbar } from 'components/Navbar';
-import { useState } from 'react';
+
+import { getUser } from '../apis/user';
+import { userState } from '../recoil/user';
 
 function Home() {
-  const [isModalVisible, setIsModalVisible] = useState(true);
+  const setUser = useSetRecoilState(userState);
+  const user = useRecoilValue(userState);
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      getUser(token)
+        .then((res) => {
+          console.log('getUser:', res.data);
+          setUser({ email: res.data.email });
+        })
+        .catch((error) => {
+          if (error instanceof AxiosError) console.log('AxiosError:', error);
+          else console.log('Not AxiosError:', error);
+        });
+    }
+  }, []);
+
+  console.log('user:', user);
 
   return (
     <div>
       <Navbar />
       <Outlet />
-      <ArticleModal
-        isModalVisible={isModalVisible}
-        setIsModalVisible={setIsModalVisible}
-      />
     </div>
   );
 }
