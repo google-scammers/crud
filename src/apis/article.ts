0b9e7deaadd5b97f3ee90bd88c164edfd9da192c
threehHong <honghyohun@naver.com>
2023-07-22 12:32:34 +0900

    feat: uploadArticle api 추가

--- src/apis/article.ts
+++ src/apis/article.ts
@@ -1,20 +1,36 @@
 import { AxiosResponse } from 'axios';
 
 import { axiosInstance } from './config';
+import { Temp } from './user';
 
 export type Article = {
-  title: string;
+  id: number;
   author: string;
+  title: string;
+  content: string;
+  created_at: Date;
 };
 
-/* 
-async는 비동기 처리할 때 사용하는 키워드
-promise를 리턴한다.
-*/
+// ArticleList
 export const getArticle = async (): Promise<AxiosResponse<Article[]>> => {
   const res = await axiosInstance({
     method: 'GET',
     url: 'article',
+  });
+  return res;
+};
+
+// uploadArticle
+export const uploadArticle = async (
+  data: FormData
+): Promise<AxiosResponse<Temp>> => {
+  const res = await axiosInstance({
+    url: 'article',
+    method: 'POST',
+    data,
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
   return res;
 };
